77635344914f3f40a845ee1cce047f4f506b3ecf
Abbas Nazari <49541849+abbasnazari-0@users.noreply.github.com>
2023-03-28 15:57:11 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,8 +2,20 @@ from flask import Flask, render_template, request
 import subprocess
 import os
 app = Flask(__name__)
+import random
 
-
+def get_random_port():
+    used_ports = []
+    if os.path.exists('usedport.txt'):
+        with open('usedport.txt', 'r') as f:
+            used_ports = f.read().splitlines()
+    while True:
+        port = random.randint(1024, 65535)
+        if str(port) not in used_ports:
+            used_ports.append(str(port))
+            with open('usedport.txt', 'w') as f:
+                f.write('\n'.join(used_ports))
+            return port
 # 14ba08eef1d6e9293390a1969023cd1e
 # generate random string for secret key
 def generate_secret_key():
@@ -21,7 +33,7 @@ def rootPage():
 
 @app.route('/re', methods=['GET', 'POST'])
 def re():
-     return (generate_secret_key())
+     return str(get_random_port()) + ' is your port, please save it for later use.'
     
 @app.route('/create', methods=['GET', 'POST'])
 def create_user():
@@ -39,10 +51,14 @@ def create_user():
     # if( withAd != None):
     #     args = ['bash', 'mtp_install.sh', '-p', '4425', '-s', generate_secret_key(), '-a', 'dd', '-a', 'tls', '-d', 'dl1.pubgtool.world', '-y']
     # else:
-    args = ['bash', 'mtp_install.sh', '-p', '4425', '-s', generate_secret_key(), '-t', 'bffb6a3d571717bd2bb14b11145d2a50', '-a', 'dd', '-a', 'tls', '-d', 'dl1.pubgtool.world', '-y']
     
-    subprocess.run(args)
-    return args
+    secret = generate_secret_key()
+    args = ['bash', 'mtp_install.sh', '-p', get_random_port(), '-s', secret, '-t', 'bffb6a3d571717bd2bb14b11145d2a50', '-a', 'dd', '-a', 'tls', '-d', 'dl1.pubgtool.world', '-y']
+    
+    # run args and return output 
+    data = subprocess.run(args, stdout=subprocess.PIPE)
+     
+    return "dd" + secret  +  " is your secret key, please save it for later use."
 
 
 if __name__ == '__main__':
